565ea1e5842c9e0568f4e702a9f8bde1b9a24d47
Johan Mårtenson <johan.martenson@gmail.com>
2014-07-11 13:06:26 +0200

    Improved handling of roads. Better drawing.

--- src/main/java/org/appland/settlers/javaview/App.java
+++ src/main/java/org/appland/settlers/javaview/App.java
@@ -6,18 +6,18 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Stroke;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import static java.lang.Math.abs;
-import static java.lang.Math.abs;
 import static java.lang.Math.abs;
 import static java.lang.Math.ceil;
 import static java.lang.Math.floor;
 import static java.lang.Math.round;
-import static java.lang.Math.round;
-import static java.lang.Math.round;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.*;
@@ -29,14 +29,19 @@ import org.appland.settlers.model.GameMap;
 import org.appland.settlers.model.Headquarter;
 import org.appland.settlers.model.Point;
 import org.appland.settlers.model.Road;
+import org.appland.settlers.model.Size;
+import static org.appland.settlers.model.Size.LARGE;
+import static org.appland.settlers.model.Size.MEDIUM;
 
 public class App {
 
-    static class GameCanvas extends Canvas implements MouseListener {
+    static class GameCanvas extends Canvas implements MouseListener, KeyListener {
 
         private UiState state;
         private List<Point> roadPoints;
-
+        private boolean showAvailableSpots;
+        private String  apiRecording;
+        
         private boolean isDoubleClick(MouseEvent me) {
             return me.getClickCount() > 1;
         }
@@ -85,18 +90,64 @@ public class App {
             addRoadPoint(p);
         }
 
+        private String recordPoint(Point p) {
+            return "new Point(" + p.x +", " + p.y + ")";
+        }
+        
         private void buildRoad(List<Point> wayPoints) throws Exception {
             System.out.println("BUILDING ROAD WITH POINTS " + wayPoints);
             
             map.placeRoad(wayPoints);
+            
+            apiRecording = apiRecording + "map.placeRoad(";
+            
+            boolean firstRun = true;
+            
+            for (Point p : wayPoints) {
+                if (firstRun) {
+                    firstRun = false;
+                    
+                    apiRecording += recordPoint(p);
+                    
+                    continue;
+                }
+                
+                apiRecording += ", " + recordPoint(p);
+            }
+            
+            apiRecording = apiRecording + ");\n";
         
             state = IDLE;
 
             roadPoints = new ArrayList<>();
         }
 
-        private void addRoadPoint(Point p) {
-            roadPoints.add(p);
+        private void addRoadPoint(Point point) {
+            if (roadPoints.isEmpty()) {
+                roadPoints.add(point);
+            } else {
+                Point last = getLastSelectedWayPoint();
+
+                if (!point.isAdjacent(last)) {
+                    System.out.println("NOT ADJACENT");
+                    
+                    List<Point> pointsBetween = map.findAutoSelectedRoad(last, point, roadPoints);
+                    
+                    boolean firstRun = true;
+                    
+                    for (Point p : pointsBetween) {
+                        if (firstRun) {
+                            firstRun = false;
+                            continue;
+                        }
+
+                        roadPoints.add(p);
+                    }
+                } else {
+                    roadPoints.add(point);
+                }
+            }
+
             repaint();
         }
 
@@ -202,6 +253,96 @@ public class App {
             return roadPoints.get(roadPoints.size() - 1);
         }
 
+        @Override
+        public void keyTyped(KeyEvent ke) {
+        }
+
+        @Override
+        public void keyPressed(KeyEvent ke) {
+            System.out.println("ANY KEY PRESSED " + ke.getKeyChar());
+            
+            if (ke.getKeyChar() == ' ') {
+                System.out.println("SPACE PRESSED");
+                
+                showAvailableSpots = !showAvailableSpots;
+                
+                repaint();
+            } else if (ke.getKeyChar() == 'd') {
+                System.out.println("--------------------------------------");
+                System.out.println(apiRecording);
+                System.out.println("--------------------------------------");
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent ke) {
+        }
+
+        private void drawAvailableSpots(Graphics2D g) {
+            Map<Point, Size> houses = map.getAvailableHousePoints();
+            List<Point> flags = map.getAvailableFlagPoints();
+            
+            for (Entry<Point, Size> pair : houses.entrySet()) {
+                drawAvailableHouse(g, pair.getKey(), pair.getValue());
+            }
+        
+            for (Point p : flags) {
+                if (houses.keySet().contains(p)) {
+                    continue;
+                }
+            
+                drawAvailableFlag(g, p);
+            }
+        }
+
+        private void placeFlag(Point p) throws Exception {
+            Flag f = new Flag(p);
+                            
+            map.placeFlag(f);
+            
+            apiRecording = apiRecording + "map.placeFlag(new Flag(new Point(" + p.x + ", " + p.y + ")));\n";
+        }
+
+        private void drawAvailableFlag(Graphics2D g, Point p) {
+            g.setColor(Color.ORANGE);
+
+            fillScaledRect(g, p, 2, 2);
+        }
+        
+
+        private void drawAvailableHouse(Graphics2D g, Point key, Size value) {
+            int height = 3;
+            
+            g.setColor(Color.YELLOW);
+            
+            if (value == MEDIUM) {
+                height = 5;
+            } else if (value == LARGE) {
+                height = 9;
+            }
+            
+            fillScaledRect(g, key, 3, height);
+        }
+
+        private String recordPointList(List<Point> wayPoints) {
+            String result = "Arrays.asList(new Point[] {";
+
+            boolean firstRun = true;
+            
+            for (Point p : wayPoints) {
+                if (firstRun) {
+                    result = result + "new Point(" + p.x + ", " + p.y + ")";
+                    firstRun = false;
+                } else {
+                    result = result + ", new Point(" + p.x + ", " + p.y + ")";
+                }
+            }
+            
+            result = result + "})";
+
+            return result;
+        }
+
         enum UiState {
 
             IDLE, BUILDING_ROAD
@@ -214,7 +355,6 @@ public class App {
         List<Point> grid;
         int scaleX;
         int scaleY;
-        Point click;
 
         public GameCanvas() {
         }
@@ -224,17 +364,24 @@ public class App {
 
             width = w;
             height = h;
-            click = null;
             roadPoints = new ArrayList<>();
+            showAvailableSpots = false;
+            apiRecording = "";
 
             /* Create the initial game board */
             map = new GameMap(width, height);
 
+            apiRecording = apiRecording + "GameMap map = new GameMap(" + w + ", " + h + ");\n";
+            
             Headquarter hq = new Headquarter();
+            
             Point hqPoint = new Point(5, 5);
 
             map.placeBuilding(hq, hqPoint);
 
+            apiRecording = apiRecording + "map.placeBuilding(new Headquarter(), new Point(" + 5 + ", " + 5 + "));\n";
+
+            
             grid = buildGrid(width, height);
 
             scaleX = 500 / width;
@@ -242,7 +389,8 @@ public class App {
 
             /* Create listener */
             addMouseListener(this);
-
+            addKeyListener(this);
+            
             /* Initial state is IDLE */
             state = IDLE;
         }
@@ -283,6 +431,10 @@ public class App {
 
             drawHouses(g);
 
+            if (showAvailableSpots) {
+                drawAvailableSpots(g);
+            }
+            
             if (state == BUILDING_ROAD) {
                 drawPreliminaryRoad(g);
 
@@ -339,14 +491,12 @@ public class App {
         @Override
         public void mouseClicked(MouseEvent me) {
             Point p = screenToPoint(me.getX(), me.getY());
-            System.out.println("Clicked at " + me.getX() + ", " + me.getY() + " - " + p);
-
-            if (click == null) {
-                click = new Point(me.getX(), me.getY());
+            
+            if (isDoubleClick(me)) {
+                System.out.println("Double click at" + me.getX() + ", " + me.getY() + " - " + p);
+            } else {
+                System.out.println("Single click at" + me.getX() + ", " + me.getY() + " - " + p);
             }
-
-            click.x = me.getX();
-            click.y = me.getY();
 
             try {
 
@@ -359,18 +509,14 @@ public class App {
 
                             state = BUILDING_ROAD;
                         } else {
-                            Flag f = new Flag(p);
-                            
-                            map.placeFlag(f);
+                            placeFlag(p);
                             
                             System.out.println("Placed flag at " + p);
                         }
                     } else if (state == BUILDING_ROAD) {
                         System.out.println("Placing flag at " + p);
                         
-                        Flag f = new Flag(p);
-                        
-                        map.placeFlag(f);
+                        placeFlag(p);
                         
                         System.out.println("Building road " + roadPoints);
                         buildRoad(roadPoints);
@@ -418,14 +564,14 @@ public class App {
         GameCanvas canvas = new GameCanvas();
 
         try {
-            canvas.initGame(30, 30);
+            canvas.initGame(20, 20);
         } catch (Exception e) {
             System.out.println(e);
         }
 
         JFrame frame = new JFrame();
 
-        frame.setSize(500, 500);
+        frame.setSize(600, 500);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
